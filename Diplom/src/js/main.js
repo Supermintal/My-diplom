32c835ab40ce0a8b6f2cdd941c6269e82e73e8c1
Supermintal <50400448+Supermintal@users.noreply.github.com>
2019-06-18 21:03:50 +0300

    Диплом
    почти сделал

---
+++ Diplom/src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+		$('.slider').slick({
+			// slidesToShow: 1,
+			// slidesToScroll: 1,
+			// autoplay: true,
+			// autoplaySpeed: 5000,
+			arrows: false
+		});
+});
---
+++ Diplom/src/js/map.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+// {/* <script type = "text/javascript"
+// charset = "utf-8"
+// async src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Abe201369a2616f31fc7f61911711eace9a9a699863db3e5c7ff6f4fd3d9b5577&amp;width=100%25&amp;height=666&amp;lang=ru_RU&amp;scroll=true"></script> */}
+
+// });
